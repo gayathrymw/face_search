06a46e3b744e0e98912f9c93adaf78bd3984f867
Gayathry Wariyar <gayathrymw@gmail.com>
2023-08-25 11:10:54 +0530

    img

--- FACE_REC/face_rec_SIFT.py
+++ FACE_REC/face_rec_SIFT.py
@@ -2,9 +2,21 @@ import cv2
 import numpy as np
 import os
 
+# Function to detect and crop face from an image
+def detect_and_crop_face(image, face_cascade):
+    faces = face_cascade.detectMultiScale(image, 1.3, 5)
+    if len(faces) == 0:
+        return image
+    x, y, w, h = faces[0]
+    return image[y:y+h, x:x+w]
+
+# Load the face cascade for face detection
+face_cascade = cv2.CascadeClassifier('FACE_REC/haar_face.xml')
+
 # Load the input image
-input_image = cv2.imread('FACE_REC/eval/2023-08-21-105001.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('Input image', input_image)
+input_image = cv2.imread('FACE_REC/eval/2023-08-21-104257.jpg', cv2.IMREAD_GRAYSCALE)
+cropped_input = detect_and_crop_face(input_image, face_cascade)
+cv2.imshow('Cropped Input', cropped_input)
 
 # Get a list of dataset image paths from the 'dataset' folder
 dataset_folder = 'FACE_REC/dataset/new_db'
@@ -13,8 +25,8 @@ dataset_image_paths = [os.path.join(dataset_folder, filename) for filename in os
 # Initialize SIFT detector and descriptor
 sift = cv2.SIFT_create()
 
-# Find the keypoints and descriptors for the input image
-keypoints_input, descriptors_input = sift.detectAndCompute(input_image, None)
+# Find the keypoints and descriptors for the cropped input image
+keypoints_input, descriptors_input = sift.detectAndCompute(cropped_input, None)
 
 # Initialize a brute-force matcher
 bf = cv2.BFMatcher()
@@ -35,7 +47,7 @@ for dataset_image_path in dataset_image_paths:
     # Apply ratio test to find good matches
     good_matches = []
     for m, n in matches:
-        if m.distance < 0.70 * n.distance:
+        if m.distance < 0.75 * n.distance:
             good_matches.append(m)
     
     # Calculate the distance of good matches
@@ -50,7 +62,7 @@ for dataset_image_path in dataset_image_paths:
         best_match_image = dataset_image
 
 # Display the best match image
-best_match_image = cv2.resize(best_match_image, (500, 500))
+best_match_image = cv2.resize(best_match_image, (700, 700))
 cv2.imshow('Best Match Image', best_match_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
