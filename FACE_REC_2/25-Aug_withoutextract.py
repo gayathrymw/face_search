da71372e2a610403cad620c593a9f8ef3c416d25
Gayathry Wariyar <gayathrymw@gmail.com>
2023-08-31 12:53:34 +0530

    images

--- FACE_REC_2/25-Aug_withoutextract.py
+++ FACE_REC_2/25-Aug_withoutextract.py
@@ -43,7 +43,7 @@ def match_faces(embeddings, query_embedding):
 def main():
     dataset_dir = 'dataset'
     embeddings_dir = 'data/embeddings'
-    query_image_path = 'eval/2023-08-25-132449.jpg'
+    query_image_path = 'eval/173f7b14e9.jpg'
 
     face_detector = dlib.get_frontal_face_detector()
     face_recognizer = dlib.face_recognition_model_v1('model/data')
